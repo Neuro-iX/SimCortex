db58ff4f2b05e8584bad5224fb472d434d891dd3
Kaveh Moradkhani <Kaveh-Moradkhani@users.noreply.github.com>
2026-09-03 09:40:08 -0400

    feat: expose package version

---
+++ src/simcortex/__init__.py
@@ -0,0 +1,10 @@
+"""SimCortex package."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("simcortex")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
+
+__all__ = ["__version__"]
